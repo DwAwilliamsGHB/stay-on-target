d04a321a747ecffcfff31904480f76662a071257
DwAwilliams23 <120130350+DwAwilliams23@users.noreply.github.com>
2023-03-10 04:28:22 -0500

    Setting up basic home page design and implementing API

---
+++ src/components/Exercises/Exercises.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Exercises() {
+  
+  return (
+    <div>Exercises</div>
+  )
+}
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,15 @@
-import './App.css';
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { getUser } from '../../utilities/users-service'
+import { Box } from '@mui/material'
+import './App.css';
+
 import AuthPage from '../AuthPage/AuthPage';
-import AboutPage from '../AboutPage/AboutPage';
-import HomePage from '../HomePage/HomePage';
 import NavBar from '../../components/NavBar/NavBar'
+import Home from '../Home/Home';
+import ExerciseDetail from '../ExerciseDetail/ExerciseDetail'
+import About from '../About/About';
+import Footer from '../../components/Footer/Footer';
 
 export default function App() {
   const [ user, setUser ] = useState(getUser())
@@ -14,13 +18,15 @@ export default function App() {
     <main className="App">
       {
         user ?
-        <>
+        <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
           <NavBar user={user} setUser={setUser} />
           <Routes>
-            <Route path="/home/about" element={<AboutPage />} />
-            <Route path="/home" element={<HomePage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise/:id" element={< ExerciseDetail />}/>
           </Routes>
-        </>
+          <Footer/>
+        </Box>
         :
         <AuthPage setUser={setUser} />
       }
